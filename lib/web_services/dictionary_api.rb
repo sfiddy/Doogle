6cb3c4a4954d0f9c71862dc55de89dab68b1a3fb
Stephanie Fiddy <stephanie.fiddy@hp.com>
2018-10-03 18:53:08 +0000

    Added require_relative to words controller

--- lib/web_services/dictionary_api.rb
+++ lib/web_services/dictionary_api.rb
@@ -1,55 +1,57 @@
-class WebServices::DictionaryApi
-    
-  def initialize(term)
-    @term = term
-  end
-  
-  def self.base_url 
-    "https://www.dictionaryapi.com/api/v1/references/collegiate/xml/"
-  end
-  
-  def full_url
-    "#{DictionaryApi.base_url}#{@term}?key=#{ENV['DICTIONARY_API_KEY_DEV']}"
-  end
-  
-  
-  def fetch_definitions 
-    @response = Faraday.get full_url
-  end
-  
-  def all_definitions
-    @response = fetch_definitions
-    @payload  = Nokogiri::XML(@response.body)
-    @definitions = parse_definitions_payload(@payload)
-  end
-  
-  
-  def get_pronunciation
-    @response = fetch_definitions
-    @payload  = Nokogiri::XML(@response.body)
-    @pronunciation = parse_payload_for_pronunciation(@payload)
-    @pronunciation
-  end
-  
-  private
-    def parse_definitions_payload(payload)
-      first_entry = payload.xpath("//entry").first
+module WebServices
+  class DictionaryApi
       
-      @definitions = Array.new
-      
-      if first_entry
-        first_entry.children.xpath("dt").each do |d|
-          definition = d.text.gsub!(/^:/, '')
-          @definitions.push(definition) # if definition != nil
-        end          
-      end
-      
-      @definitions
+    def initialize(term)
+      @term = term
     end
     
-    def parse_payload_for_pronunciation(payload)
-      first_entry = payload.xpath("//entry").first
-      @pronunciation = first_entry.xpath("pr").text
+    def self.base_url 
+      "https://www.dictionaryapi.com/api/v1/references/collegiate/xml/"
+    end
+    
+    def full_url
+      "#{DictionaryApi.base_url}#{@term}?key=#{ENV['DICTIONARY_API_KEY_DEV']}"
+    end
+    
+    
+    def fetch_definitions 
+      @response = Faraday.get full_url
+    end
+    
+    def all_definitions
+      @response = fetch_definitions
+      @payload  = Nokogiri::XML(@response.body)
+      @definitions = parse_definitions_payload(@payload)
+    end
+    
+    
+    def get_pronunciation
+      @response = fetch_definitions
+      @payload  = Nokogiri::XML(@response.body)
+      @pronunciation = parse_payload_for_pronunciation(@payload)
       @pronunciation
     end
+    
+    private
+      def parse_definitions_payload(payload)
+        first_entry = payload.xpath("//entry").first
+        
+        @definitions = Array.new
+        
+        if first_entry
+          first_entry.children.xpath("dt").each do |d|
+            definition = d.text.gsub!(/^:/, '')
+            @definitions.push(definition) # if definition != nil
+          end          
+        end
+        
+        @definitions
+      end
+      
+      def parse_payload_for_pronunciation(payload)
+        first_entry = payload.xpath("//entry").first
+        @pronunciation = first_entry.xpath("pr").text
+        @pronunciation
+      end
+  end
 end
